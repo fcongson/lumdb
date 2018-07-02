7975579f2c1423ca5abd501d35737ccc993eb235
Frank Congson <fcongson@gmail.com>
2018-07-02 22:53:17 +1200

    Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Movie from "./Movie";
+import Movie from './Movie';
 
 class App extends Component {
   state = {
-    movies: []
+    movies: [],
   };
 
   async componentDidMount() {
     try {
       const res = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=16574c6a75d0ee6843ffd3d9ca95b91e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+        'https://api.themoviedb.org/3/discover/movie?api_key=16574c6a75d0ee6843ffd3d9ca95b91e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
       );
       const movies = await res.json();
       this.setState({
-        movies: movies.results
+        movies: movies.results,
       });
     } catch (e) {
       console.log(e);
@@ -24,12 +24,13 @@ class App extends Component {
   }
 
   render() {
+    const { movies } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
